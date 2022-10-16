74cd645fe214ed86c368d8241ca93ad2f4421eb6
Raheel Sangrasi <115358662+RaheelSangrasi@users.noreply.github.com>
2022-10-16 09:49:26 +0500

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,20 @@
+//Create a program using every single operator we have learned Still now
+import java.util.*;
+class arithmetic
+{
+public static void main (Strring[]args)
+{
+double a;
+double b;
+Scanner obj=new Scanner(System.in);
+System.out.println("Enter Number");
+a=obj.nextDouble();
+System.out.println("Enter Number");
+b=obj.nextDouble();
+System.out.println("The addition of two number is "+(a+b));
+System.out.println("The subtraction of two number is "+(a-b));
+System.out.println("The multiplication of two number is"+(a*b));
+System.out.println("The division of two number is"+(a/b));
+System.out.println("The modulus of two number is"+(a%b));
+}
+}
