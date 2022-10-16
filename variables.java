9dc5c216bee3cfcfb39e71f760c9fb7d3173e97f
Raheel Sangrasi <115358662+RaheelSangrasi@users.noreply.github.com>
2022-10-16 09:50:58 +0500

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,20 @@
+class variable
+{
+    public static void main(String[]args)
+    {
+        int a=9;
+        int b=7;
+        System.out.println(a);
+        System.out.println(b);
+    double c=8.4;
+    double d=0.9;
+    System.out.println(c);
+    System.out.println(d);
+    char e='g';
+    char f='h';
+    System.out.println(e);
+    System.out.println(f);
+    String t="Java programs";
+    System.out.println("t");
+    }
+}
