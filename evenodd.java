c43569a4c92583c06de4fe326e1232670dc2cff5
Raheel Sangrasi <115358662+RaheelSangrasi@users.noreply.github.com>
2022-10-16 09:50:26 +0500

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class evenodd
+{
+public static void main(String[]args)
+{
+System.out.println("Enter the number");
+Scanner obj=new scanner(System.in);
+int a=obj.nextInt();
+if(a%2==0)
+{
+System.out.println("Number is even");
+}
+else
+{
+System.out.println("Number is odd");
+}
+}
+}
